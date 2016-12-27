cfc3f3d23f03607cbe8914ae8c27010836f2b951
gpr95 <grzesgrzes95@wp.pl>
2016-12-27 19:51:17 +0100

    exception dialog + css design

--- src/sample/controller/MainApp.java
+++ src/sample/controller/MainApp.java
@@ -1,16 +1,24 @@
 package sample.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 import sample.model.Bus;
 import sample.model.Drive;
@@ -28,6 +36,12 @@ public class MainApp extends Application {
     private ObservableList<Bus> busData = FXCollections.observableArrayList();
     //private ObservableList<IntermediateDrive> intermediateDrivesData = FXCollections.observableArrayList();
     private DataBaseHandler dbh;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    
     public MainApp()
     {
     	dbh = new DataBaseHandler();
@@ -58,7 +72,7 @@ public class MainApp extends Application {
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
-            e.printStackTrace();
+        	exceptionDialog(e);
         }
     }
 
@@ -71,7 +85,7 @@ public class MainApp extends Application {
             rootLayout.setCenter(tabbedLayout);
 
         } catch (IOException e) {
-            e.printStackTrace();
+        	exceptionDialog(e);
         }
     }
 
@@ -89,7 +103,7 @@ public class MainApp extends Application {
             DriveOverviewController controller = loader.getController();
             controller.setMainApp(this);
         } catch (IOException e) {
-            e.printStackTrace();
+        	exceptionDialog(e);
         }
     }
 
@@ -102,7 +116,7 @@ public class MainApp extends Application {
             BusOverviewController controller = loader.getController();
             controller.setMainApp(this);
         } catch (IOException e) {
-            e.printStackTrace();
+        	exceptionDialog(e);
         }
     }
 
@@ -122,7 +136,7 @@ public class MainApp extends Application {
 
             return controller.isOkClicked();
         } catch (IOException e) {
-            e.printStackTrace();
+        	exceptionDialog(e);
             return false;
         }
     }
@@ -132,10 +146,6 @@ public class MainApp extends Application {
      */
     public Stage getPrimaryStage() {
         return primaryStage;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
     public ObservableList<Drive> getDriveData() {
@@ -156,5 +166,41 @@ public class MainApp extends Application {
 
     public ObservableList<Bus> getBusData() {
         return busData;
+    }
+    
+    
+    public void exceptionDialog(Exception e)
+    {
+    	Alert alert = new Alert(AlertType.ERROR);
+    	alert.setTitle("Exception Dialog");
+    	alert.setHeaderText("Look, an Exception Dialog");
+    	alert.setContentText("Error has occured");
+
+    	// Create expandable Exception.
+    	StringWriter sw = new StringWriter();
+    	PrintWriter pw = new PrintWriter(sw);
+    	e.printStackTrace(pw);
+    	String exceptionText = sw.toString();
+
+    	Label label = new Label("The exception stacktrace was:");
+
+    	TextArea textArea = new TextArea(exceptionText);
+    	textArea.setEditable(false);
+    	textArea.setWrapText(true);
+
+    	textArea.setMaxWidth(Double.MAX_VALUE);
+    	textArea.setMaxHeight(Double.MAX_VALUE);
+    	GridPane.setVgrow(textArea, Priority.ALWAYS);
+    	GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+    	GridPane expContent = new GridPane();
+    	expContent.setMaxWidth(Double.MAX_VALUE);
+    	expContent.add(label, 0, 0);
+    	expContent.add(textArea, 0, 1);
+
+    	// Set expandable Exception into the dialog pane.
+    	alert.getDialogPane().setExpandableContent(expContent);
+
+    	alert.showAndWait();
     }
 }
